f9abf5ca98518a48ee467ab26bc451ed58a74b2b
nurtulu <nurtulu.dev@gmail.com>
2024-05-30 18:37:47 +0300

    latest file

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const counterElement = document.getElementById('counter');
-    const incrementButton = document.getElementById('increment-btn');
-    let counter = 0;
+let counter = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
+document.getElementById('counter').innerText = counter;
 
-    incrementButton.addEventListener('click', () => {
-        counter++;
-        counterElement.textContent = counter;
-    });
-});
+function increaseCounter() {
+    counter++;
+    document.getElementById('counter').innerText = counter;
+    localStorage.setItem('counter', counter);
+}
+
+function resetCounter() {
+    counter = 0;
+    document.getElementById('counter').innerText = counter;
+    localStorage.setItem('counter', counter);
+}
